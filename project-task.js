a4e8df1c8191d3d3ee548698d44ed8684d9a299d
Jaya <jaya.kolte21@gmail.com>
2025-05-21 21:36:07 -0500

    Practice submission

--- project-task.js
+++ project-task.js
@@ -45,19 +45,35 @@ Step 4: Test Your Solution
 function processFile(fileName, fileData) {
   try {
     // TODO: Add input validation here
-    
+    if (!fileName) {
+      throw new ReferenceError("File name is missing");
+    }
+
+    if (typeof fileData !== "string") {
+      throw new TypeError("File data must be a string");
+    }
+    if (fileData.trim() === "") {
+      throw new Error("File data cannot be empty");
+    }
+ 
     // TODO: Implement simulated file processing here
     console.log(`Processing file: ${fileName}`);
     console.log(`File content: ${fileData}`);
     
     // TODO: Add simulated file operations (reading/writing)
+    console.log("Reading file...");
+    console.log("Writing file...");
     
   } catch (err) {
     // TODO: Implement error handling
-    console.error(err);
-  }
+    console.error(` Error: ${err.message}`);  }
   // TODO: Implement a finally block to close resources
+  finally {
+    // Always runs
+    console.log(" Closing file resources...\n");
+  }
 }
+
 
 // ============================================
 // 🧪 Test Cases Below
